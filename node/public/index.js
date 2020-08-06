cdcf425c572cda2cc74115a3991f8090e73cf714
Han Han <Haaannnnn@hotmail.com>
2020-08-06 02:27:00 +0000

    fix bug and display answer and time

--- node/public/index.js
+++ node/public/index.js
@@ -2,18 +2,21 @@ let engineSelect = document.getElementById("engine-select");
 let modeSelect = document.getElementById("mode-select");
 let maxNumber = document.getElementById("max-number");
 let submitButton = document.getElementById("submit-button");
+let resultP = document.getElementById('result')
 
 submitButton.addEventListener("click", (e) => {
   axios
     .post("/solve", {
       "engine-select": engineSelect.value,
       "mode-select": modeSelect.value,
-      maxNumber: maxNumber.value === "" ? 2 : parseInt(maxNumber.value),
+      "max-number": maxNumber.value === "" ? 2 : parseInt(maxNumber.value),
     })
     .then((res) => {
       console.log(res);
+      resultP.innerText = `计算结果：${res.data.answer}，花费时间：${parseFloat(res.data.time) / 1000}`
     })
     .catch((err) => {
       console.err(err);
+      resultP.innerText = `出错啦：${JSON.stringify(err)}`
     });
 });
